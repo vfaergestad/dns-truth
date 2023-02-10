4493f8197061cbce32d110ba9b4631fbe8fd6841
vegard <vgf@hotmail.no>
2023-02-10 20:46:46 +0100

    Added file for custom errors

---
+++ internal/custom_errors/custom_errors.go
@@ -0,0 +1,8 @@
+package custom_errors
+
+const (
+	// ErrInvalidBackendType is returned when the backend type is invalid
+	ErrInvalidBackendType = "invalid backend type"
+	ErrNoHostnameInFormat = "no hostname in format"
+	ErrNoIpInFormat       = "no IP in format"
+)
