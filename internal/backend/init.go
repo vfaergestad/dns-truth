628f16ed3411fcbeb0eb910795ef53a5ed649087
vegard <vgf@hotmail.no>
2023-02-10 20:47:16 +0100

    Added testing-lines

--- internal/backend/init.go
+++ internal/backend/init.go
@@ -1,17 +1,27 @@
 package backend
 
 import (
-	"dns-truth/internal/backend/local_file"
 	"dns-truth/internal/backend/records"
+	"dns-truth/internal/backend/types/local_file"
+	"fmt"
 	"os"
 )
 
 func Init() {
 	backendType := os.Getenv("BACKEND_TYPE")
 	if backendType == "local_file" {
-		local_file.Init()
+		err := local_file.Init()
+		if err != nil {
+			panic(err)
+		}
 	} else {
 		panic("Invalid backend type")
 	}
 	records.PrintRecords()
+	fmt.Println("Testing change of records")
+	recordList := records.GetRecords()
+	err := local_file.UpdateRecords(recordList)
+	if err != nil {
+		panic(err)
+	}
 }
